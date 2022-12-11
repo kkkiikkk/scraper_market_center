17792d4b4454f9251e700c4d2ff366fb58668f55
kkkiikkk <ruddnovskiy@gmail.com>
2022-12-11 10:14:09 -0800

    feat: setup receiving information

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log(1)
+// Core
+import axios from 'axios'
+
+// Config
+import config from './config/idnex'
+
+const axiosInstance = axios.create()
+
+const main = async () => {
+    try {
+        const response = await axiosInstance.get(config.PARSE_URL)
+        console.log(response.data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main()
+    .then()
+    .catch((error) => {
+        console.log('ERROR', error)
+    })
